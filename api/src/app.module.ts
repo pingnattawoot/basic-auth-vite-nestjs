a8d3baa898819661c7584d0ed17554463ad7656a
pingnattawoot <nattawoot.sara@gmail.com>
2025-01-09 18:54:44 +0700

    add database configuration

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
@@ -9,6 +11,23 @@ import { AuthModule } from './auth/auth.module';
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.getOrThrow<string>('DB_HOST'),
+        port: configService.getOrThrow<number>('DB_PORT'),
+        username: configService.getOrThrow<string>('DB_USERNAME'),
+        password: configService.getOrThrow<string>('DB_PASSWORD'),
+        database: configService.getOrThrow<string>('DB_DATABASE'),
+        synchronize: false,
+        logging: false,
+        timezone: 'Z',
+        entities: [join(__dirname, '../database/entities/**/*{.ts,.js}')],
+        migrations: [join(__dirname, '../database/migrations/**/*{.ts,.js}')],
+      }),
+      inject: [ConfigService],
     }),
     AuthModule,
   ],
